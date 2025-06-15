b1e7cac885941c3fc1d6ac7d4a59e1f9cb677236
martijn <martijnvwezel@muino.nl>
2025-06-15 22:12:21 +0200

    Update __init__.py

--- esphome/components/muino_3phase_i2c/__init__.py
+++ esphome/components/muino_3phase_i2c/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@hugokernel"]
+CODEOWNERS = ["@martijnvwezel", "@hugokernel"]
 DEPENDENCIES = ["i2c"]
 AUTO_LOAD = ["sensor"]
 
